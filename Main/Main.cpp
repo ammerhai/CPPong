c224762838f7690dd886297e264c19240b509c38
Ammerhai <mikebalzer@aol.com>
2020-01-30 18:11:28 +0100

    #4 added Points, #5 added Ball movement and collision and Pong-MiddleLine

--- Main/Main.cpp
+++ Main/Main.cpp
@@ -42,32 +42,6 @@ struct PixelColor {
 	uint8 a;
 };
 
-
-void renderQuad(V2 pos, V2 halfsize, V3 color) {
-	V2 topleft = pos - halfsize;
-	V2 bottomright = pos + halfsize;
-	
-	topleft = clamp01(topleft);
-	bottomright = clamp01(bottomright);
-
-	M2x2 projection = {
-		BitmapWidth, 0.0f,
-		0.0f, BitmapHeight
-	};
-
-	topleft = projection * topleft;
-	bottomright = projection * bottomright;
-	for (int y = topleft.y; y < bottomright.y; y++) {
-		for (int x = topleft.x; x < bottomright.x; x++) {
-			PixelColor* pixel = (PixelColor*)BitmapMemory + x + y * BitmapWidth;
-			pixel->b = 255 * color.b;
-			pixel->g = 255 * color.g;
-			pixel->r = 255 * color.r;
-			pixel->a = 0xFF;
-		}
-	}
-}
-
 //function to learn rendering
 internal_function void RenderGradient(int XOffset, int YOffset) {
 
@@ -101,27 +75,193 @@ internal_function void RenderGradient(int XOffset, int YOffset) {
 }
 
 //Spieler 1 Attribute
-V2 pos_p1 = { 0.1, 0.5 };
-V2 size_p1 = {0.01, 0.07};
-V3 color_p1 = { 0.9, 1, 0.5 };
+V2 p1_pos = { 0.1, 0.5 };
+V2 p1_size = {0.01, 0.07};
+V3 p1_color = { 0.9, 1, 0.5 };
+int p1_points = 0;
 
 //Spieler 2 Attribute
-V2 pos_p2 = { 0.9, 0.5 };
-V2 size_p2 = { 0.01, 0.07 };
-V3 color_p2 = { 0.5, 1, 0.9 };
+V2 p2_pos = { 0.9, 0.5 };
+V2 p2_size = { 0.01, 0.07 };
+V3 p2_color = { 0.5, 1, 0.9 };
+int p2_points = 0;
 
 //Ball Attribute
-V2 pos_ball = { 0.5, 0.5 };
-V2 size_ball = { 0.01, 0.01 };
-V3 color_ball = {1, 0, 0};
-float speed_ball = 0.4;
-V2 direction_ball = normalize(V2{1, 0.5});
+V2 ball_pos = { 0.5, 0.5 };
+V2 ball_size = { 0.01, 0.01 };
+V3 ball_color = {1, 0, 0};
+float ball_speed = 0.5;
+V2 ball_direction = normalize(V2{-1, 0.9});
 
 //Gameplay Attribute
 float player_speed = 0.5;
 V2 border = { 0.01, 0.01 } ;
 bool key_down[256];
 double last_time = get_time();
+
+//Rendering der einzelnen Quadrate mit Farbgebung
+void renderQuad(V2 pos, V2 halfsize, V3 color) {
+	V2 topleft = pos - halfsize;
+	V2 bottomright = pos + halfsize;
+
+	topleft = clamp01(topleft);
+	bottomright = clamp01(bottomright);
+
+	M2x2 projection = {
+		(float)BitmapWidth, 0.0f,
+		0.0f, (float)BitmapHeight
+	};
+
+	topleft = projection * topleft;
+	bottomright = projection * bottomright;
+	for (int y = topleft.y; y < bottomright.y; y++) {
+		for (int x = topleft.x; x < bottomright.x; x++) {
+			PixelColor* pixel = (PixelColor*)BitmapMemory + x + y * BitmapWidth;
+			pixel->b = 255 * color.b;
+			pixel->g = 255 * color.g;
+			pixel->r = 255 * color.r;
+			pixel->a = 0xFF;
+		}
+	}
+}
+
+void render_point(int p1_points, int p2_points) {
+	//Punktanzeige für P1
+	switch (p1_points) {
+		case 0: printf("0\n");
+			renderQuad({ 0.8, 0.1 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.77, 0.2 }, { 0.01, 0.1 }, { 1, 1, 1 });
+			renderQuad({ 0.83, 0.2 }, { 0.01, 0.1 }, { 1, 1, 1 });
+			renderQuad({ 0.8, 0.3 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			break;
+		case 1: printf("1\n");
+			renderQuad({ 0.83, 0.2 }, { 0.01, 0.1 }, { 1, 1, 1 });
+			break;
+		case 2: printf("2\n");
+			renderQuad({ 0.8, 0.1 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.83, 0.15 }, { 0.01, 0.05 }, { 1, 1, 1 });
+			renderQuad({ 0.8, 0.2 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.77, 0.25 }, { 0.01, 0.05 }, { 1, 1, 1 });
+			renderQuad({ 0.8, 0.3 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			break;
+		case 3: printf("3\n");
+			renderQuad({ 0.8, 0.1 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.83, 0.15 }, { 0.01, 0.05 }, { 1, 1, 1 });
+			renderQuad({ 0.8, 0.2 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.83, 0.25 }, { 0.01, 0.05 }, { 1, 1, 1 });
+			renderQuad({ 0.8, 0.3 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			break;
+		case 4: printf("4\n");
+			renderQuad({ 0.83, 0.2 }, { 0.01, 0.1 }, { 1, 1, 1 });
+			renderQuad({ 0.8, 0.2 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.77, 0.15 }, { 0.01, 0.05 }, { 1, 1, 1 });
+			break;
+		case 5: printf("5\n");
+			renderQuad({ 0.8, 0.1 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.77, 0.15 }, { 0.01, 0.05 }, { 1, 1, 1 });
+			renderQuad({ 0.8, 0.2 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.83, 0.25 }, { 0.01, 0.05 }, { 1, 1, 1 });
+			renderQuad({ 0.8, 0.3 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			break;
+		case 6: printf("6\n");
+			renderQuad({ 0.77, 0.2 }, { 0.01, 0.1 }, { 1, 1, 1 });
+			renderQuad({ 0.8, 0.2 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.83, 0.25 }, { 0.01, 0.05 }, { 1, 1, 1 });
+			renderQuad({ 0.8, 0.3 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			break;
+		case 7: printf("7\n");
+			renderQuad({ 0.8, 0.1 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.83, 0.2 }, { 0.01, 0.1 }, { 1, 1, 1 });
+			break;
+		case 8: printf("8\n");
+			renderQuad({ 0.8, 0.1 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.77, 0.2 }, { 0.01, 0.1 }, { 1, 1, 1 });
+			renderQuad({ 0.8, 0.2 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.83, 0.2 }, { 0.01, 0.1 }, { 1, 1, 1 });
+			renderQuad({ 0.8, 0.3 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			break;
+		case 9: printf("9\n");
+			renderQuad({ 0.8, 0.1 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.77, 0.15 }, { 0.01, 0.05 }, { 1, 1, 1 });
+			renderQuad({ 0.8, 0.2 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.83, 0.2 }, { 0.01, 0.1 }, { 1, 1, 1 });
+			break;
+	}
+
+	//Punktanzeige für P2
+	switch (p2_points) {
+		case 0: printf("0:");
+			renderQuad({ 0.2, 0.1 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.17, 0.2 }, { 0.01, 0.1 }, { 1, 1, 1 });
+			renderQuad({ 0.23, 0.2 }, { 0.01, 0.1 }, { 1, 1, 1 });
+			renderQuad({ 0.2, 0.3 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			break;
+		case 1: printf("1:");
+			renderQuad({ 0.23, 0.2 }, { 0.01, 0.1 }, { 1, 1, 1 });
+			break;
+		case 2: printf("2:");
+			renderQuad({ 0.2, 0.1 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.23, 0.15 }, { 0.01, 0.05 }, { 1, 1, 1 });
+			renderQuad({ 0.2, 0.2 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.17, 0.25 }, { 0.01, 0.05 }, { 1, 1, 1 });
+			renderQuad({ 0.2, 0.3 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			break;
+		case 3: printf("3:");
+			renderQuad({ 0.2, 0.1 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.23, 0.15 }, { 0.01, 0.05 }, { 1, 1, 1 });
+			renderQuad({ 0.2, 0.2 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.23, 0.25 }, { 0.01, 0.05 }, { 1, 1, 1 });
+			renderQuad({ 0.2, 0.3 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			break;
+		case 4: printf("4:");
+			renderQuad({ 0.23, 0.2 }, { 0.01, 0.1 }, { 1, 1, 1 });
+			renderQuad({ 0.2, 0.2 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.17, 0.15 }, { 0.01, 0.05 }, { 1, 1, 1 });
+			break;
+		case 5: printf("5:");
+			renderQuad({ 0.2, 0.1 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.17, 0.15 }, { 0.01, 0.05 }, { 1, 1, 1 });
+			renderQuad({ 0.2, 0.2 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.23, 0.25 }, { 0.01, 0.05 }, { 1, 1, 1 });
+			renderQuad({ 0.2, 0.3 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			break;
+		case 6: printf("6:");
+			renderQuad({ 0.17, 0.2 }, { 0.01, 0.1 }, { 1, 1, 1 });
+			renderQuad({ 0.2, 0.2 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.23, 0.25 }, { 0.01, 0.05 }, { 1, 1, 1 });
+			renderQuad({ 0.2, 0.3 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			break;
+		case 7: printf("7:");
+			renderQuad({ 0.2, 0.1 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.23, 0.2 }, { 0.01, 0.1 }, { 1, 1, 1 });
+			break;
+		case 8: printf("8:");
+			renderQuad({ 0.2, 0.1 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.17, 0.2 }, { 0.01, 0.1 }, { 1, 1, 1 });
+			renderQuad({ 0.2, 0.2 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.23, 0.2 }, { 0.01, 0.1 }, { 1, 1, 1 });
+			renderQuad({ 0.2, 0.3 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			break;
+		case 9: printf("9:");
+			renderQuad({ 0.2, 0.1 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.17, 0.15 }, { 0.01, 0.05 }, { 1, 1, 1 });
+			renderQuad({ 0.2, 0.2 }, { 0.04, 0.01 }, { 1, 1, 1 });
+			renderQuad({ 0.23, 0.2 }, { 0.01, 0.1 }, { 1, 1, 1 });
+			break;
+	}
+}
+
+
+void render_middle_line() {
+	for (float a = 0.02f; a <= 1;) {
+		renderQuad({ 0.5, a }, { 0.001, 0.01 }, { 1, 1, 1 });
+		a = a + 0.08;
+	}
+}
+
+void Reset_Ball() {
+	ball_pos = { 0.5, 0.5 };
+}
 
 struct Minkowski_sum {
 	V2 pos;
@@ -164,65 +304,92 @@ float Minkowski_intersects_ray(Minkowski_sum sum, V2 origin, V2 dir) {
 	return tmin;
 }
 
+V2 move_ball(V2 ball_pos, V2 &ball_direction, float distance_to_travel) {
+	if (distance_to_travel == 0)
+		return ball_pos;
+
+	auto p1 = Minkowski_sum(p1_pos, p1_size, ball_size);
+	auto p2 = Minkowski_sum(p2_pos, p2_size, ball_size);
+	auto wall_top = Minkowski_sum({ 0.5, 0 }, { 1, 0 }, ball_size);
+	auto wall_bottom = Minkowski_sum({ 0.5, 1 }, { 1, 0 }, ball_size);
+
+	float distance_top = clamp(0, Minkowski_intersects_ray(wall_top, ball_pos, ball_direction), INFINITY);
+	float distance_bottom = clamp(0, Minkowski_intersects_ray(wall_bottom, ball_pos, ball_direction), INFINITY);
+	float distance_p1 = clamp(0, Minkowski_intersects_ray(p1, ball_pos, ball_direction), INFINITY);
+	float distance_p2 = clamp(0, Minkowski_intersects_ray(p2, ball_pos, ball_direction), INFINITY);
+
+	distance_top = distance_top == 0.0f ? INFINITY : distance_top;
+	distance_bottom = distance_bottom == 0.0f ? INFINITY : distance_bottom;	
+	distance_p1 = distance_p1 == 0.0f ? INFINITY : distance_p1;
+	distance_p2 = distance_p2 == 0.0f ? INFINITY : distance_p2;
+
+	float distance_min = min(min(distance_p1, distance_p2), min(distance_top, distance_bottom));
+
+	if (distance_min >= distance_to_travel)
+		return ball_pos + ball_direction * distance_to_travel;
+
+	ball_pos = ball_pos + ball_direction * distance_min;
+
+	if (distance_min == distance_p1)
+		ball_direction.x = fabs(ball_direction.x);
+	if (distance_min == distance_p2)
+		ball_direction.x = -fabs(ball_direction.x);
+	if (distance_min == distance_top)
+		ball_direction.y = fabs(ball_direction.y);
+	if (distance_min == distance_bottom)
+		ball_direction.y = -fabs(ball_direction.y);
+
+	return move_ball(ball_pos, ball_direction, distance_to_travel - distance_min);
+}
+
 void UpdatePong() {
 
-#define left(a) (pos_##a .x - size_##a .x)
-#define right(a) (pos_##a .x + size_##a .x)
-#define top(a) (pos_##a .y - size_##a .y)
-#define bottom(a) (pos_##a .y + size_##a .y)
+#define left(a) (a ##_pos.x - a ##_size.x)
+#define right(a) (a ##_pos.x + a ##_size.x)
+#define top(a) (a ##_pos.y - a ##_size.y)
+#define bottom(a) (a ##_pos.y + a ##_size.y)
 
 	double current_time = get_time();
 	double delta_time = current_time - last_time;
 
 	//Steuerung Spieler 1
 	if (key_down['W']) {
-		pos_p1.y = pos_p1.y - player_speed * delta_time;
-		pos_p1 = clamp01(pos_p1 - size_p1 - border) + size_p1 + border;
+		p1_pos.y = p1_pos.y - player_speed * delta_time;
+		p1_pos = clamp01(p1_pos - p1_size - border) + p1_size + border;
 	}
 	if (key_down['S']) {
-		pos_p1.y = pos_p1.y + player_speed * delta_time;
-		pos_p1 = clamp01(pos_p1 + size_p1 + border) - size_p1 - border;
+		p1_pos.y = p1_pos.y + player_speed * delta_time;
+		p1_pos = clamp01(p1_pos + p1_size + border) - p1_size - border;
 	}
 
 	//Steuerung Spieler 2
 	if (key_down[VK_UP]) {
-		pos_p2.y = pos_p2.y - player_speed * delta_time;
-		pos_p2 = clamp01(pos_p2 - size_p2 - border) + size_p2 + border;
+		p2_pos.y = p2_pos.y - player_speed * delta_time;
+		p2_pos = clamp01(p2_pos - p2_size - border) + p2_size + border;
 	}
 	if (key_down[VK_DOWN]) {
-		pos_p2.y = pos_p2.y + player_speed * delta_time;
-		pos_p2 = clamp01(pos_p2 + size_p2 + border) - size_p2 - border;
+		p2_pos.y = p2_pos.y + player_speed * delta_time;
+		p2_pos = clamp01(p2_pos + p2_size + border) - p2_size - border;
 	}
 
-	auto p1 = Minkowski_sum(pos_p1, size_p1, size_ball);
-	auto p2 = Minkowski_sum(pos_p2, size_p2, size_ball);
+	float distance_traveled = ball_speed * delta_time;
 
-	//Berührung Rand oben
-	if (top(ball) < 0) {
-		direction_ball.y = fabs(direction_ball.y);
+	ball_pos = move_ball(ball_pos, ball_direction, distance_traveled);
+
+	//Punkte
+	if (left(ball) < 0 && p1_points < 9) {
+		p1_points = p1_points + 1;
+		Reset_Ball();
+	}
+	if (right(ball) > 1 && p2_points < 9) {
+		p2_points = p2_points + 1;
+		Reset_Ball();
 	}
 
-	//Berührung Rand unten
-	if (bottom(ball) > 1) {
-		direction_ball.y = -fabs(direction_ball.y);
-	}
+	//Farbänderung, TODO: nach v1.0 -> Items, wird aktiviert
+	ball_color.r = sin(fmod(current_time, 6.28318530718));
 
-	float distance = 0.0f;
-	//Berührung Spieler 1
-	if((distance = Minkowski_intersects_ray(p1, pos_ball, direction_ball)) != 0.0f)
-		if(distance < speed_ball * delta_time)
-			direction_ball.x = fabs(direction_ball.x);
-	
-	//Berührung Spieler 2
-	if ((distance = Minkowski_intersects_ray(p2, pos_ball, direction_ball)) != 0.0f)
-		if (distance < speed_ball * delta_time)
-			direction_ball.x = -fabs(direction_ball.x);
-
-	//Bewegung des Balles
-	pos_ball = pos_ball + direction_ball * speed_ball * delta_time;
-
-
-	color_ball.r = sin(fmod(current_time, 6.28318530718));
+	//printf("frame time: %.5g fps: %.3g\n", (current_time - last_time), 1.0 / (current_time - last_time));
 
 	last_time = current_time;
 }
@@ -231,14 +398,14 @@ void RenderPong() {
 	UpdatePong();
 
 	float ratio = (float)BitmapWidth / (float)BitmapHeight;
-	size_ball.y = size_ball.x * ratio;
-
+	ball_size.y = ball_size.x * ratio;
 
 	memset(BitmapMemory, 0, BitmapWidth * BitmapHeight * BytesPerPixel);
-	renderQuad(pos_p1, size_p1, color_p1);
-	renderQuad(pos_p2, size_p2, color_p2);
-	renderQuad(pos_ball, size_ball, color_ball);
-
+	render_middle_line();
+	render_point(p1_points, p2_points);
+	renderQuad(p1_pos, p1_size, p1_color);
+	renderQuad(p2_pos, p2_size, p2_color);
+	renderQuad(ball_pos, ball_size, ball_color);
 }
 
 
@@ -258,7 +425,6 @@ internal_function void ResizeDIBSection(int Width, int Height) {
 	BitmapInfo.bmiHeader.biBitCount = 32;
 	BitmapInfo.bmiHeader.biCompression = BI_RGB;
 
-	//int BytesPerPixel = 4;
 	int BitmapMemorySize = (BitmapWidth * BitmapHeight) * BytesPerPixel;
 	BitmapMemory = VirtualAlloc(0, BitmapMemorySize, MEM_COMMIT, PAGE_READWRITE);
 
